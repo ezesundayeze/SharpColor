09eb14dfdfdedc63a1fa8e8beca7a251ec19f490
Eze Sunday <rexeze@gmail.com>
2016-08-14 16:43:07 -1200

    initial

---
+++ SharpColor/SharpColor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+
+namespace SharpColor
+{
+    public class SharpColor
+    {
+        //Colors
+        public Color SharpRed { get { return Color.FromArgb(255, 0, 0); } }
+        public Color Sharporange { get { return Color.FromArgb(255, 0, 0); } }
+        public Color SharpBlue { get { return Color.FromArgb(100, 170, 200); } }
+        public Color SharpPurple { get { return Color.FromArgb(255, 0, 0); } }
+        public Color SharpGreen { get { return Color.FromArgb(255, 0, 0); } }
+    }
+}
